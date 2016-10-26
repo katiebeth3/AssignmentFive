a0c8eacd8209db38835b6b83436a7b8e4671a20f
Katie Rios <katiebeth3@comcast.net>
2016-10-25 23:15:17 -0600

    Finished

--- AssignmentFive/Controllers/HomeController.cs
+++ AssignmentFive/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AssignmentFive.DAL;
+using AssignmentFive.ViewModels;
 
 namespace AssignmentFive.Controllers
 {
     public class HomeController : Controller
     {
+        private ActivityDBContext db = new ActivityDBContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +19,16 @@ namespace AssignmentFive.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            {
+                IQueryable<LeaderActivityData> data = from tripleader in db.Activities
+                                                       group tripleader by tripleader.TripLeader.LastName into dateGroup
+                                                       select new LeaderActivityData()
+                                                       {
+                                                           TripLeaderLastName = dateGroup.Key,
+                                                           ActivityCount = dateGroup.Count()
+                                                       };
+                return View(data.ToList());
+            }
         }
 
         public ActionResult Contact()
@@ -25,6 +36,12 @@ namespace AssignmentFive.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
